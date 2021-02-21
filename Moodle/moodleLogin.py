6ee7f3654e234e2f1701516a8d667028566fded8
Deepit Patil <deepitpatil@Deepits-MacBook-Pro.local>
2021-02-21 18:23:45 +0530

    Moodle auto-login with user input

--- Moodle/moodleLogin.py
+++ Moodle/moodleLogin.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
-username = "am1200648"
+username = input("username : ")
 password = input("password : ")
 
 driver = webdriver.Chrome("/Users/deepitpatil/chromedriver")
